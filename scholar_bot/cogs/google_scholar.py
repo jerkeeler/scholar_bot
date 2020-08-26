52dc781f6a8c1285c87bc968f8fc259692b987a0
Jeremy Keeler <j@keeler.dev>
2020-08-26 13:29:11 -0400

    Remove log file and update help

--- scholar_bot/cogs/google_scholar.py
+++ scholar_bot/cogs/google_scholar.py
@@ -5,10 +5,19 @@ from scholarly import scholarly
 class GoogleScholarCog(commands.Cog):
     @commands.command()
     async def gscholar(self, ctx, *args):
+        """UNDER DEVELOPMENT - Retrieve list of recent papers by an author"""
         author_name = " ".join(args).lower()
-        # search_query = scholarly.search_author(author_name)
+        search_query = scholarly.search_author(author_name)
+        try:
+            author = next(search_query)
+        except StopIteration:
+            ctx.send(f"Author {author_name} not found!")
+            return
         await ctx.send(f"Searching for: {author_name}")
 
     @commands.command()
     async def hello(self, ctx):
+        """
+        Says hello!
+        """
         await ctx.send("Hello!")
--- scholar_bot/cogs/sci_hub.py
+++ scholar_bot/cogs/sci_hub.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 class SciHubCog(commands.Cog):
     @commands.command()
     async def paper(self, ctx, *args):
+        """UNDER DEVELOPMENT - Retrieve a paper from SciHub"""
         paper_title = " ".join(args).lower()
         await ctx.send(f"Serching SciHub for: {paper_title}")
 
